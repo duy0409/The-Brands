805f26f163e222d0c454875ef539cc29491c0727
Test <Nguyentanchieu2002@gmail.com>
2024-03-16 02:36:15 +0700

    first commit

---
+++ src/app/components/Carousel.jsx
@@ -0,0 +1,31 @@
+// components/Carousel.js
+import React from 'react';
+
+//npm install react-slick slick-carousel
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Carousel = ({ slides }) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
+
+    return (
+        <Slider {...settings}>
+            {slides.map((slide, index) => (
+                <div key={index}>
+                    <img src={slide} alt={`Slide ${index + 1}`} />
+                </div>
+            ))}
+        </Slider>
+    );
+};
+
+export default Carousel;
