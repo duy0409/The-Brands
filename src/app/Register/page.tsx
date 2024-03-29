98066863e9fd2ed595fec7a5d939e0d7823ae31a
Test <Nguyentanchieu2002@gmail.com>
2024-03-29 10:49:35 +0700

    29032024

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
     const [inputType, setInputType] = useState('password');
 
     const togglePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -17,9 +17,9 @@ const Login = () => {
     return (
         <div className={Style.loginPage}>
 
-            <Link href="/"><Image className={Style.image} src="/FashionNews/logo.svg" alt="logo" width={190} height={40} /></Link>
+            <Image className={Style.image} src="/FashionNews/logo.svg" alt="logo" width={190} height={40} />
             <div className={Style.container}>
-                <a id={Style.title}>Login</a><br /><br />
+                <a id={Style.title}>Register</a><br /><br />
                 <form className={Style.bodyContent}>
                     <label htmlFor="email" className={Style.label}>User name or Email:</label>
                     <input className={Style.inputE} id="email" name="email" /><br />
@@ -27,17 +27,17 @@ const Login = () => {
                     <div className={Style.fromPassword}>
                         <input className={Style.inputP} type={inputType} id="password" name="password" />
                         <button className={Style.eye} onClick={togglePasswordVisibility}>{showPassword ? <Image src="/login/eye.svg" alt="eye" width={22} height={22} /> : <Image src="/login/eyeOff.svg" alt="eye" width={22} height={22} />}</button></div>
-
+                    <label htmlFor="password" className={Style.label}>Confirm Password:</label>
+                    <div className={Style.fromPassword}>
+                        <input className={Style.inputP} type={inputType} id="password" name="password" />
+                        <button className={Style.eye} onClick={togglePasswordVisibility}>{showPassword ? <Image src="/login/eye.svg" alt="eye" width={22} height={22} /> : <Image src="/login/eyeOff.svg" alt="eye" width={22} height={22} />}</button></div>
                 </form><br />
-                <div className={Style.container1}>
-                    <Link href="#" className={Style.fgPassword}>Forgot Password?</Link>
-                </div>
-                <br /><div>
-                    <button className={Style.btnSignIn}>Sign in</button>
+                <div>
+                    <button className={Style.btnSignIn}>Sign up</button>
                 </div>
                 <br /><div className={Style.linkHere}>
-                    <a >Don’t have an account?</a>
-                    <a href='/Register' id={Style.here}> Sign up here</a>
+                    <a >Already have an account? </a>
+                    <a href='/Login' id={Style.here}> Login</a>
                 </div>
             </div>
 
