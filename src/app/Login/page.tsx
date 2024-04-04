efa23acf1d1d3ca595ddb54ad6873dbb4a7e9e69
Test <Nguyentanchieu2002@gmail.com>
2024-04-04 14:54:11 +0700

    shttps://github.com/TanChieu/Ayden_Brand.git

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import React, { useEffect, useState } from 'react';
-
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Style from "../css/Login.module.css";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,60 +8,72 @@ import Link from "next/link";
 const Login = () => {
     const [showPassword, setShowPassword] = useState(true);
     const [inputType, setInputType] = useState('password');
-
-    const togglePasswordVisibility = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        e.preventDefault();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
+    const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
         setInputType(showPassword ? 'text' : 'password');
     };
-
-
-    useEffect(() => {
-        // (async () => {
-        //     const response = await fetch('/api/login');
-        //     const body = await response.json()
-        //     console.log("body", body)
-        // })()
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    };
+    const handleSignIn = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
         (async () => {
-            const response = await fetch('/api/login', {
-                method: "POST",
-                body: JSON.stringify({ messages: "Hello" })
-            });
-            const { data } = await response.json()
-            if (data.isLogged) {
-                alert("Dang nhap")
+            try {
+                const response = await fetch('/api/login', {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ email, password })
+                });
+                const data = await response.json();
+                if (data.isLogged) {
+                    alert("Login successful");
+                    router.push('/');
+                } else {
+                    alert("Login failed");
+                }
+            } catch (error) {
+                console.error('Error:', error);
             }
-        })()
-    }, [])
-
-
+        })();
+    };
     return (
         <div className={Style.loginPage}>
-
             <Link href="/"><Image className={Style.image} src="/FashionNews/logo.svg" alt="logo" width={190} height={40} /></Link>
             <div className={Style.container}>
                 <a id={Style.title}>Login</a><br /><br /><br /><br />
                 <form className={Style.bodyContent}>
                     <label htmlFor="email" className={Style.label}>Email:</label>
-                    <input className={Style.inputE} id="email" name="email" /><br />
+                    <input className={Style.inputE} id="email" name="email" value={email} onChange={handleEmailChange} /><br />
                     <label htmlFor="password" className={Style.label}>Password:</label>
                     <div className={Style.fromPassword}>
-                        <input className={Style.inputP} type={inputType} id="password" name="password" />
-                        <button className={Style.eye} onClick={togglePasswordVisibility}>{showPassword ? <Image src="/login/eye.svg" alt="eye" width={22} height={22} /> : <Image src="/login/eyeOff.svg" alt="eye" width={22} height={22} />}</button></div>
+                        <input className={Style.inputP} type={inputType} id="password" name="password" value={password} onChange={handlePasswordChange} />
+                        <button className={Style.eye} onClick={togglePasswordVisibility}>{showPassword ? <Image src="/login/eye.svg" alt="eye" width={22} height={22} />
+                            : <Image src="/login/eyeOff.svg" alt="eye" width={22} height={22} />}</button>
+                    </div>
                 </form><br />
                 <div className={Style.container1}>
                     <Link href="#" className={Style.fgPassword}>Forgot Password?</Link>
                 </div>
-                <br /><div>
-                    <button className={Style.btnSignIn}>Sign in</button>
+                <br />
+                <div>
+                    <button className={Style.btnSignIn} type="submit" onClick={handleSignIn}>Sign in</button>
+
                 </div>
                 <br /><div className={Style.linkHere}>
                     <a >Don’t have an account?</a>
                     <Link href='/Register' id={Style.here}> Sign up here</Link>
                 </div>
             </div>
-
         </div>
     );
 };
+
 export default Login;
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,22 @@
+
+import fakeJson from "../config/login.json"
+
 export async function GET() {
 
   return Response.json({ data: { isLogged: true } })
 }
 
 export async function POST(req: Request) {
-  const { messages } = await req.json()
-
-  return Response.json({ data: { isLogged: messages === "1111" } })
+  const { email, password } = await req.json()
+  try {
+    const user = fakeJson.find((user: { email: any; password: any; }) => user.email === email && user.password === password);
+    if (user) {
+      return Response.json({ isLogged: true });
+    } else {
+      return Response.json({ isLogged: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: 'Internal server error' });
+  }
 }
